58abeddbeb6800b6a2a7402812f487eb462ff4c0
Dong Xuan Toan <strangersuntoan@gmail.com>
2018-03-22 07:55:28 +0700

    Create load.py

---
+++ load.py
@@ -0,0 +1,64 @@
+'''Trains a simple convnet on the MNIST dataset.
+Gets to 99.25% test accuracy after 12 epochs
+(there is still a lot of margin for parameter tuning).
+16 seconds per epoch on a GRID K520 GPU.
+'''
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential,load_model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import cv2 as cv
+import matplotlib.pyplot as plt
+batch_size = 128
+num_classes = 10
+epochs = 1
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+model=Sequential() 
+'''
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=(28,28,1)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+model.load_weights('weight.h5')'''
+model=load_model('model.h5')
+img=cv.imread('3.jpg')
+img2=cv.resize(img,(28,28));
+gray=cv.cvtColor(img2,cv.COLOR_BGR2GRAY);
+plt.imshow(gray)
+plt.show()
+test=gray.reshape(1,28,28,1)
+res=model.predict(test,batch_size=batch_size,verbose=1)
+print(res[0])
+'''x_test=x_test.reshape(10000,28,28,1)
+res=model.predict(x_test,batch_size=batch_size,verbose=1)
+x_test=x_test.reshape(10000,28,28)
+plt.imshow(x_test[0])
+plt.show()
+print(res[0])'''
